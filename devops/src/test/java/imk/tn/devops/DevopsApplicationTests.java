d3bf9d35c1ce5061303130cfeb91c3db8720fe08
Issaoui Med5 <imk.med5@gmail.com>
2020-05-20 23:34:16 +0300

    add project

---
+++ devops/src/test/java/imk/tn/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package imk.tn.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
