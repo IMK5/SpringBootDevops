ef3886a8713860a8a12870966b4a7a3df948e8b3
IMK5 <61726156+IMK5@users.noreply.github.com>
2020-05-21 03:19:11 +0300

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -24,9 +24,4 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
+
