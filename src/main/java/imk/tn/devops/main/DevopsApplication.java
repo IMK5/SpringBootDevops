62b634da33aea03717aa0175a583679025880b72
IMK5 <61726156+IMK5@users.noreply.github.com>
2020-06-08 17:26:46 +0300

    Update DevopsApplication.java

--- src/main/java/imk/tn/devops/main/DevopsApplication.java
+++ src/main/java/imk/tn/devops/main/DevopsApplication.java
@@ -1,12 +1,21 @@
 package imk.tn.devops.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevopsApplication {
+public class DevopsApplication extends SpringBootServletInitializer {
+// SpringBootServletInitializer used to deploy springBoot app in tomcat container
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(DevopsApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsApplication.class, args);
 	}
 
 }
+
